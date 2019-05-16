4b1e74eed8b0f286665e95f341b0925403f0cd05
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2019-05-16 09:26:13 -0400

    Working sync for one file

--- syncfile/sync.py
+++ syncfile/sync.py
@@ -1,13 +1,18 @@
-import config
+import config as cfg
 
 from ftplib import FTP
 
 ftp = FTP(cfg.server)
-ftp.login(cfg.user, cfg.password)
-
-open(cfg.local_file_directory+cfg.filename,'rb')                # file to send
-ftp.cwd(cfg.remote_file_directory)
-ftp.storbinary(cfg.filename, file)                              # send the file
+ftp.login(cfg.ftp_credentials['user'], cfg.ftp_credentials['password'])
+try:
+    file = open(cfg.local_file_directory+cfg.filename,'rb')                # file to send
+except FileNotFoundError:
+    print("Local file not found. Check config!")
+try:
+    ftp.cwd(cfg.remote_file_directory)
+except:
+    print("Remote directory not found. Check config")
+ftp.storbinary("STOR "+cfg.filename, file) 
 file.close()                                                    # close file and FTP
 ftp.quit()
 
